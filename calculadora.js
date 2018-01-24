e92a87c0b679310adb089018c456a308a050fa6e
makireland <makireland@gmail.com>
2018-01-24 02:32:10 +0000

    onclick button name created

--- calculadora.js
+++ calculadora.js
@@ -5,7 +5,9 @@ var Validation = function (fieldId) {
 Validation.prototype.add = function (field){
     var val = document.getElementById(this.fieldId).value;
     var val2 = document.getElementById(field).value;
-    
+    var resultado = val + val2;
+    console.log("Resultado da Adicao" + resultado);
+
 }
 
 Validation.prototype.subtraction = function (field){
