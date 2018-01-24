88271e066996adf069b1ba67eada987512753e7b
makireland <makireland@gmail.com>
2018-01-24 02:07:30 +0000

    methods were created

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+var Validation = function (fieldId) {
+    this.fieldId = fieldId;
+}
+
+Validation.prototype.add = function (field){
+    var val = document.getElementById(this.fieldId).value;
+    var val2 = document.getElementById(field).value;
+}
+
+Validation.prototype.subtraction = function (field){
+    var val = document.getElementById(this.fieldId).value;
+    var val2 = document.getElementById(field).value;
+}
+
+Validation.prototype.multiply = function (field){
+    var val = document.getElementById(this.fieldId).value;
+    var val2 = document.getElementById(field).value;
+}
+
+Validation.prototype.division = function (field){
+    var val = document.getElementById(this.fieldId).value;
+    var val2 = document.getElementById(field).value;
+}
